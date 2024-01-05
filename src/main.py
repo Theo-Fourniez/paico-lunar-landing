16fc8d378e1b03624e4c4e0e27378bd291f1719a
Theo-Fourniez <10551899+Theo-Fourniez@users.noreply.github.com>
2024-01-05 19:18:43 +0100

    feat: added printing messages

--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
 import gymnasium as gym
 env = gym.make("LunarLander-v2", render_mode="human")
 observation, info = env.reset()
+episode = 0
 
-for _ in range(1000):
+print("Go go go")
+for _ in range(5555):
     action = env.action_space.sample()  # agent policy that uses the observation and info
     observation, reward, terminated, truncated, info = env.step(action)
-
     if terminated or truncated:
+        episode += 1
+        print(f"Episode {episode} finished")
         observation, info = env.reset()
 
+print("Terminating environment")
 env.close()
